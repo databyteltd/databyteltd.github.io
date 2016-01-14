85c51a481ce7f986cf73b342eb96ef48e384c90c
databyteltd <chriswang@databyte.com.tw>
2016-01-14 16:12:48 +0800

    0114

--- databyte/javascript/productindexlv2.js
+++
@@ -1,5 +0,0 @@
-var screenwidth = screen.width;
-var producticon = document.getElementById("producticon");
-if (screenwidth > 500) {
-    producticon.setAttribute("href", "../productindex.html");
-}
