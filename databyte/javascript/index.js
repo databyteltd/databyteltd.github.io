45bc1789b3d7480217515c9b85216dcb4043b01a
databyteltd <chriswang@databyte.com.tw>
2016-01-15 11:05:59 +0800

    0115

--- databyte/javascript/index.js
+++ databyte/javascript/index.js
@@ -6,6 +6,14 @@ var itemnav = document.getElementById("itemnav");
 var navframe = document.getElementById("navframe");
 var navicon = document.getElementById("navicon");
 var naviconlist = document.getElementById("naviconlist");
+var mask = document.getElementById("mask");
+var body = document.getElementById("body");
+var iconremove = document.getElementById("iconremove");
+
+function navanibackfunction() {
+    naviconlist.style.display = "none";
+}
+
 if (screenwidth > 500) {
     producticon.setAttribute("href", "pages/productindex.html");
 }
@@ -14,22 +22,49 @@ if (screenwidth < 500) {
     new2.innerHTML = "Our new development, Hub/Charger Series, has pure hub version, pur charger version and hub+charger version. These three categories share the same feature: 1. USB3.0 Super-speed ports which......";
     new3.innerHTML = "HDS2511 is our best single bay product this year. It has so many great features. 1. HDS251 has a Hi-Tech texture appearance made by its Carbon-Fibers stylish design and it can be anti-fingerprint......";
     itemnav.style.display = "none";
-    navicon.style.display = "block";
-    navicon.style.fontSize = "35px";
-    navicon.style.fontWeight = "bold";
-    navicon.style.color = "#005CAF";
-    navicon.style.position = "absolute";
-    navicon.style.right = "5%";
-    navicon.style.top = "10px";
     navicon.addEventListener("click", function() {
-
         if (naviconlist.style.display == "block") {
-            naviconlist.style.display = "none";
+            //naviconlist.style.display = "none";
+            mask.style.display = "none";
+            navicon.style.animationName = "naviconback";
+            navicon.style.animationDuration = "0.6s";
+            navicon.style.right = "5%";
+            navicon.style.color = "#005CAF";
+            navicon.style.transform = "rotate(0deg)";
+            naviconlist.style.animationName = "navaniback";
+            naviconlist.style.animationDuration = "0.6s";
+            window.setTimeout(navanibackfunction, 600);
+            navicon.style.position = "absolute";
         } else {
+            mask.style.display = "block";
             naviconlist.style.display = "block";
-            naviconlist.style.animationName = "navani";
-            naviconlist.style.animationDuration = "0.4s";
+            naviconlist.style.animationName = "navanigo";
+            naviconlist.style.animationDuration = "0.6s";
+            navicon.style.animationName = "navicongo";
+            navicon.style.animationDuration = "0.6s";
+            navicon.style.right = "34%";
+            navicon.style.color = "#113285";
+            navicon.style.transform = "rotate(90deg)";
+            navicon.style.position = "fixed";
         }
-
     });
+
+
+    iconremove.addEventListener("click", function() {
+        if (naviconlist.style.display == "block") {
+            //naviconlist.style.display = "none";
+            mask.style.display = "none";
+            navicon.style.animationName = "naviconback";
+            navicon.style.animationDuration = "0.6s";
+            navicon.style.right = "5%";
+            navicon.style.color = "#005CAF";
+            navicon.style.transform = "rotate(0deg)";
+            naviconlist.style.animationName = "navaniback";
+            naviconlist.style.animationDuration = "0.6s";
+            window.setTimeout(navanibackfunction, 600);
+            navicon.style.position = "absolute";
+        }
+    });
+
+
 }
